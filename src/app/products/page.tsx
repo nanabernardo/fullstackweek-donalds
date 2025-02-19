496096a3a625053b3303735cc5e684b205e866a2
nanabernardo <nanabernardo@gmail.com>
2025-02-19 12:06:03 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const ProdutcPage = () => {
   return (
-    <div className="p-5 border border-red-500 rounded-xl">
+    <div className="rounded-xl border border-red-500 p-5">
       <h1 className="text-red-500">Product Page</h1>
       <Button>Click me</Button>
       <Input placeholder="Bora coda!"></Input>
