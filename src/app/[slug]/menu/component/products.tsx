a3a107710a12c10bc1ec988dbf6dc95c3a5892c1
nanabernardo <nanabernardo@gmail.com>
2025-02-21 21:12:05 -0300

    feat: add products details page

--- src/app/[slug]/menu/component/products.tsx
+++ src/app/[slug]/menu/component/products.tsx
@@ -3,43 +3,46 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
- interface ProductsProps {
-    products: Product[]
- }
+import { formatCurrency } from "@/helpers/format-currency";
 
-const Products = ({products}: ProductsProps) => {
-    const { slug } = useParams<{ slug: string}>()
-    return ( 
-        <div className="space-y-3 px-5">
-            {products.map(product => (
-                <Link key={product.id} href={`/${slug}/menu/${product.id}`} className="flex items-center justify-between gap-10 border-b py-5" >
-                    {/* ESQUERDA*/}
-                    <div>
-                        <h3 className="text-sm font-medium">
-                            {product.name}
-                        </h3>
-                        <p className="line-clamp-2 text-sm text-muted-foreground">
-                            {product.description}
-                        </p>
-                        <p className="pt-3 text-sm font-semibold">{new Intl.NumberFormat("pt-BR", {
-                            style: "currency",
-                            currency: "BRL",
-                        }).format(product.price)}</p>
-                    </div>
-
-                    {/* DIREITA */}
-                    <div className="relative min-h-[82px] min-w-[120px]">
-                        <Image 
-                        src={product.imageUrl} 
-                        alt={product.name} 
-                        fill 
-                        className="rounded-lg object-contain" 
-                        />                        
-                    </div>
-                </Link>
-            ))}
-        </div>
-     );
+interface ProductsProps {
+  products: Product[];
 }
- 
+
+const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  return (
+    <div className="space-y-3 px-5">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          href={`/${slug}/menu/${product.id}`}
+          className="flex items-center justify-between gap-10 border-b py-5"
+        >
+          {/* ESQUERDA*/}
+          <div>
+            <h3 className="text-sm font-medium">{product.name}</h3>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}
+            </p>
+          </div>
+
+          {/* DIREITA */}
+          <div className="relative min-h-[82px] min-w-[120px]">
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-contain"
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
 export default Products;
